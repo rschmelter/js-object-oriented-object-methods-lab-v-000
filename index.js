2bbf31e44a68faf786721ca45ba0fd62f990ad76
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-15 22:50:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ function BoardMember (name, homeState, training) {
     return "I like to help people."
   }
 
+  this.releasePressStatement = function() {
+    return "You will see great things from Scuber."
+  }
+
 }
