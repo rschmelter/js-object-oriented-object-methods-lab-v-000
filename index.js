8a4b06a70aabf7c4df42b88f46f7a851f36582c3
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-15 22:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function BoardMember (name, homeState, training) {
   this.veto = function() {
     return "No, I must disagree"
   }
+  this.approve = function() {
+    return "You can do that!"
+  }
 
 }
