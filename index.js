ce7a5b13cbf5a95ae91586eacddc531ce98b187d
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-15 22:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,8 @@ function BoardMember (name, homeState, training) {
     return "You will see great things from Scuber."
   }
 
+  this.sayHi = function () {
+    return `Hi, my name is $(this.name). I am from $(this.homestate), and I was trained in $(this.training).`
+  }
+
 }
